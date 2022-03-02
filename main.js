dd0467213d914a4674712936333bb0b358de3553
José Lucas R. Santos <joselucasrs@gmail.com>
2022-03-01 23:58:00 -0300

    Criando preload.js e adicionando ao main.js

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+// include the Node.js 'path' module at the top of your file
+const path = require('path');
+
 const { app, BrowserWindow } = require('electron');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
   });
 
   win.loadFile('index.html');
